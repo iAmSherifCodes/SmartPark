31df3bfe906721753110fe019c2873dfe5067061
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-10-28 03:30:33 +0100

    reroute to correct link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <main className="flex mt-40 flex-col items-center justify-center ">
             <div className="absolute top-4 right-4">
                 <ModeToggle />
             </div>
@@ -21,7 +21,7 @@ export default function Home() {
             <h1 className="text-4xl font-bold mb-8 text-center">Welcome to SmartPark</h1>
 
             <div className="flex flex-col items-center space-y-3 w-60 size-11">
-                <Link href="/checkout">
+                <Link href="/availability">
                     <Button className="w-full max-w-xs">View Available Spaces {"->"}</Button>
                 </Link>
                 <div className= "flex gap-4">
